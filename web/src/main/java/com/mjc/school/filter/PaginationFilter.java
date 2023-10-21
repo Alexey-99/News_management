f5704a21374e82e281e365cdb6b74af2dbca541e
Alexey <koroza.alexey99@gmail.com>
2023-10-21 20:24:08 +0300

    *

--- web/src/main/java/com/mjc/school/filter/PaginationFilter.java
+++ web/src/main/java/com/mjc/school/filter/PaginationFilter.java
@@ -19,23 +19,12 @@ import static com.mjc.school.service.pagination.PaginationService.DEFAULT_SIZE;
 public class PaginationFilter implements Filter {
 
     @Override
-    public void doFilter(
-            ServletRequest servletRequest,
-            ServletResponse servletResponse,
-            FilterChain filterChain)
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws ServletException, IOException {
         String size = servletRequest.getParameter("size");
         String page = servletRequest.getParameter("page");
-        size = size != null &&
-                size.matches("^\\d+$") &&
-                Integer.parseInt(size) > 0 ?
-                size :
-                DEFAULT_SIZE;
-        page = page != null &&
-                page.matches("^\\d+$") &&
-                Integer.parseInt(size) > 0 ?
-                page :
-                DEFAULT_NUMBER_PAGE;
+        size = size != null && size.matches("^\\d+$") && Integer.parseInt(size) > 0 ? size : DEFAULT_SIZE;
+        page = page != null && page.matches("^\\d+$") && Integer.parseInt(size) > 0 ? page : DEFAULT_NUMBER_PAGE;
         servletRequest.setAttribute("size", size);
         servletRequest.setAttribute("page", page);
         filterChain.doFilter(servletRequest, servletResponse);
