94e20935ca5d3c1d167d681727237c7a058e2b86
Alexey <koroza.alexey99@gmail.com>
2023-10-22 19:45:06 +0300

    *

--- model/src/main/java/com/mjc/school/Author.java
+++ model/src/main/java/com/mjc/school/Author.java
@@ -13,6 +13,7 @@ import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import static javax.persistence.CascadeType.ALL;
 import static javax.persistence.GenerationType.IDENTITY;
@@ -39,12 +40,47 @@ public class Author implements Serializable {
     }
 
     @Override
+    public int hashCode() {
+        final int PRIME = 31;
+        int result = 1;
+        result = result * PRIME + Long.hashCode(this.id);
+        result = result * PRIME + (this.name != null ? this.name.hashCode() : 1);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null) {
+            return false;
+        }
+        if (!this.getClass().equals(object.getClass())) {
+            return false;
+        }
+        Author otherAuthor = (Author) object;
+        if (this.id != otherAuthor.getId()) {
+            return false;
+        }
+        if (this.name == null) {
+            if (otherAuthor.name != null) {
+                return false;
+            }
+        } else if (!this.name.equals(otherAuthor.name)) {
+            return false;
+        }
+        return true;
+    }
+
+
+    @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder();
-        sb.append("Author{");
-        sb.append("id=").append(id);
-        sb.append(", name='").append(name).append("'");
-        sb.append('}');
-        return sb.toString();
+        final StringBuilder builder = new StringBuilder();
+        builder.append("Author{");
+        builder.append("id='").append(id).append("'");
+        builder.append(", name='").append(name).append("'");
+        builder.append("}");
+        return builder.toString();
     }
 }
