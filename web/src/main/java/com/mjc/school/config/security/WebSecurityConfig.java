3a00c3c0e6b8f70b0266a4a1294eb18d180ce3df
Alexey <koroza.alexey99@gmail.com>
2023-10-25 13:47:24 +0300

    *

--- web/src/main/java/com/mjc/school/config/security/WebSecurityConfig.java
+++ web/src/main/java/com/mjc/school/config/security/WebSecurityConfig.java
@@ -1,27 +1,101 @@
 package com.mjc.school.config.security;
 
+import com.mjc.school.exception.CustomAccessDeniedHandlerImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 
+@Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService userDetailsService;
+    private final CustomAccessDeniedHandlerImpl accessDeniedHandler;
+//    private final CustomTokenFilter tokenFilter;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .cors().and().csrf().disable()
+//                .exceptionHandling()
+////                .authenticationEntryPoint(authenticationEntryPoint)   It doesn't work with oAuth2
+//                .accessDeniedHandler(accessDeniedHandler)
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/auth/**").permitAll()
+//                .antMatchers(HttpMethod.GET, "/certificate/**").permitAll()
+//                .antMatchers( "/login/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .oauth2Login();
+//        http.addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.anonymous().disable().and()
-                .anonymous(AbstractHttpConfigurer::disable)         // AnonymousAuthenticationFilter
-                .anonymous().disable()
-                .csrf(AbstractHttpConfigurer::disable)              // CsrfFilter
-                .sessionManagement(AbstractHttpConfigurer::disable) // DisableEncodeUrlFilter, SessionManagementFilter
-                .exceptionHandling(AbstractHttpConfigurer::disable) // ExceptionTranslationFilter
-                .headers(AbstractHttpConfigurer::disable)           // HeaderWriterFilter
-                .logout(AbstractHttpConfigurer::disable)            // LogoutFilter
-                .requestCache(AbstractHttpConfigurer::disable)      // RequestCacheAwareFilter
-                .servletApi(AbstractHttpConfigurer::disable)        // SecurityContextHolderAwareRequestFilter
-                .securityContext(AbstractHttpConfigurer::disable)   // SecurityContextPersistenceFilter
-                .build();
+        return http.build();
+//                http.cors().and().csrf().disable()
+//                .exceptionHandling()
+//                .accessDeniedHandler(new AccessDeniedHandler() {
+//                    @Override
+//                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+//
+//                    }
+//                })
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers().permitAll()
+//                .antMatchers(HttpMethod.GET, "/certificate/**").permitAll()
+//                .antMatchers( "/login/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .oauth2Login();
+
+//                .anonymous(AbstractHttpConfigurer::disable)         // AnonymousAuthenticationFilter
+//                .csrf(AbstractHttpConfigurer::disable)              // CsrfFilter
+//                .sessionManagement(AbstractHttpConfigurer::disable) // DisableEncodeUrlFilter, SessionManagementFilter
+//                .exceptionHandling(AbstractHttpConfigurer::disable) // ExceptionTranslationFilter
+//                .headers(AbstractHttpConfigurer::disable)           // HeaderWriterFilter
+//                .logout(AbstractHttpConfigurer::disable)            // LogoutFilter
+//                .requestCache(AbstractHttpConfigurer::disable)      // RequestCacheAwareFilter
+//                .servletApi(AbstractHttpConfigurer::disable)        // SecurityContextHolderAwareRequestFilter
+//                .securityContext(AbstractHttpConfigurer::disable)   // SecurityContextPersistenceFilter
+//                .build();
     }
 }
---
+++ web/src/main/java/com/mjc/school/filter/CustomTokenFilter.java
@@ -0,0 +1,43 @@
+package com.mjc.school.filter;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomTokenFilter extends OncePerRequestFilter {
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String PREFIX_BEARER = "Bearer ";
+
+//    private final JwtUtil jwtUtil;
+//    private final UserDetailsService userDetailsService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+//        String jwtToken = parseJwt(request);
+//        if (jwtToken != null && jwtUtil.validateToken(jwtToken)) {
+//            String userName = jwtUtil.getUserNameFromJwtToken(jwtToken);
+//            UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+//            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+//                    userDetails, null, userDetails.getAuthorities());
+//            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+//        }
+//        filterChain.doFilter(request, response);
+    }
+
+    private String parseJwt(HttpServletRequest request) {
+        String headerAuthentication = request.getHeader(AUTHORIZATION);
+        String result = null;
+        if (headerAuthentication != null &&!headerAuthentication.isBlank()
+                && headerAuthentication.startsWith(PREFIX_BEARER)) {
+            result = headerAuthentication.substring(7);
+        }
+        return result;
+    }
+}
