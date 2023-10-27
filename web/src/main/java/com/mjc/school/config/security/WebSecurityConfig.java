ba90c1291bcdef720c8f2173f25b46dd0c088565
Alexey <koroza.alexey99@gmail.com>
2023-10-27 17:23:20 +0300

    *

--- web/src/main/java/com/mjc/school/config/security/WebSecurityConfig.java
+++ web/src/main/java/com/mjc/school/config/security/WebSecurityConfig.java
@@ -2,8 +2,20 @@ package com.mjc.school.config.security;
 
 import com.mjc.school.service.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -11,18 +23,41 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 public class WebSecurityConfig {
     private final UserService userService;
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeRequests(authorizeRequests -> {
+                    authorizeRequests.antMatchers(HttpMethod.GET,
+                            "/api/v2/author/*", "/api/v2/comment/*", "/api/v2/news/*", "/api/v2/tag/*").permitAll(); //TODO: ADD ANT PATTERN REGISTRATION AND SIGN_IN FOR GUEST
+                    authorizeRequests.antMatchers(HttpMethod.POST,
+                            "/api/v2/comment/*", "/api/v2/news/*").hasRole("USER");
+                    authorizeRequests.anyRequest().hasRole("ADMIN");
+                })
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                //TODO .addFilterBefore()
+                .build();
+    }
 
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(bCryptPasswordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(userService);
+        return daoAuthenticationProvider;
+    }
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        return http.httpBasic().and()
-//                .authorizeHttpRequests(authorizeHttpRequests -> {
-//                            authorizeHttpRequests.antMatchers(HttpMethod.GET, "/api/v2/**").permitAll();
-//                            authorizeHttpRequests.anyRequest().authenticated();
-//                        }
-//                ).exceptionHandling(exceptionHandling ->
-//                        exceptionHandling.accessDeniedHandler(customAccessDeniedHandler)
-//                )
-//                .build();
-//    }
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
