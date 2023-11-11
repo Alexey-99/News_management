db1348e477df95312d1322b0c52e99b41d5d38e7
Alexey <koroza.alexey99@gmail.com>
2023-11-11 19:46:33 +0300

    *

--- service/src/test/java/com/mjc/school/converter/UserConverterTest.java
+++ service/src/test/java/com/mjc/school/converter/UserConverterTest.java
@@ -1,5 +1,20 @@
 package com.mjc.school.converter;
 
-public class UserConverterTest {
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
+@RequiredArgsConstructor
+class UserConverterTest {
+    @Autowired
+    private UserConverter userConverter;
+
+    @Test
+    void toUserDetails() {
+        assert 2 + 3 != 4;
+    }
+
+    @Test
+    void fromRegistrationUserDTO() {
+    }
 }
