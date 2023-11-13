79b15dec411abd40bfd456fe0dbf2ba4bd312447
Alexey <koroza.alexey99@gmail.com>
2023-11-13 21:20:43 +0300

    *

---
+++ service/src/test/java/com/mjc/school/handler/DateHandlerTest.java
@@ -0,0 +1,54 @@
+package com.mjc.school.handler;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
+class DateHandlerTest {
+    @InjectMocks
+    private DateHandler dateHandler;
+
+    @ParameterizedTest
+    @MethodSource(value = "providerDateTimeFormatterParams")
+    void getCurrentDate(DateTimeFormatter dateFormatter, boolean expectedResult) {
+        String dateTime = dateHandler.getCurrentDate();
+        boolean resultActual = isValid(dateTime, dateFormatter);
+        assertEquals(expectedResult, resultActual);
+    }
+
+    private boolean isValid(String str, DateTimeFormatter dateFormatter) {
+        boolean result;
+        try {
+            dateFormatter.parse(str);
+            result = true;
+        } catch (DateTimeParseException e) {
+            result = false;
+        }
+        return result;
+    }
+
+    static List<Arguments> providerDateTimeFormatterParams() {
+        return List.of(
+                Arguments.of(
+                        DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS"),
+                        true
+                ),
+                Arguments.of(
+                        DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"),
+                        false
+                )
+        );
+    }
+}
