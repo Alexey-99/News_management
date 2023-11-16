ebe31023e124006036ec94379a4df927f1fc0f67
Alexey <koroza.alexey99@gmail.com>
2023-11-16 14:11:41 +0300

    *

--- service/src/main/java/com/mjc/school/service/news/impl/sort/NewsSortField.java
+++ service/src/main/java/com/mjc/school/service/news/impl/sort/NewsSortField.java
@@ -4,14 +4,4 @@ import java.util.Optional;
 
 public enum NewsSortField {
     CREATED, MODIFIED;
-
-    public static Optional<String> getSortField(String sortField) {
-        try {
-            return sortField != null ?
-                    Optional.of(valueOf(sortField.toUpperCase()).name().toLowerCase()) :
-                    Optional.empty();
-        } catch (IllegalArgumentException e) {
-            return Optional.empty();
-        }
-    }
 }
