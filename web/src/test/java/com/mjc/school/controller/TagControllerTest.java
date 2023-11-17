3fc906d3580cf6f13e23878759928c68ac096e57
Alexey <koroza.alexey99@gmail.com>
2023-11-17 14:11:34 +0300

    *

---
+++ web/src/test/java/com/mjc/school/controller/TagControllerTest.java
@@ -0,0 +1,48 @@
+package com.mjc.school.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TagControllerTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void addToNews() {
+    }
+
+    @Test
+    void deleteFromNews() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void deleteFromAllNews() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByPartOfName() {
+    }
+
+    @Test
+    void findByNewsId() {
+    }
+}
