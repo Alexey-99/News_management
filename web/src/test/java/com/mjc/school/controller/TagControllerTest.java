a60529ac54fabb10b4a80f4ab59c634ee6f9ba2a
Alexey <koroza.alexey99@gmail.com>
2023-11-20 16:53:10 +0300

    *

--- web/src/test/java/com/mjc/school/controller/TagControllerTest.java
+++ web/src/test/java/com/mjc/school/controller/TagControllerTest.java
@@ -14,7 +14,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.util.List;
@@ -37,7 +36,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Validated
 @ExtendWith(MockitoExtension.class)
 class TagControllerTest {
     @InjectMocks
